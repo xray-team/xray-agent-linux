b2c7fe85c67d739c7f045c1647fffdf2efcf54c8
Vadym Khomenko <exsver@gmail.com>
2022-05-12 21:57:12 +0300

    move service/modules

---
+++ service/modules.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/xray-team/xray-agent-linux/logger"
+)
+
+type Module interface {
+	Start()
+	Stop()
+	Title() string
+}
+
+// RunModules runs each of the modules in a separate goroutine.
+func RunModules(modules ...Module) {
+	defer func() {
+		for _, m := range modules {
+			logger.Log.Info.Printf(logger.Message, logger.TagAgent, fmt.Sprintf("Stopping module %s", m.Title()))
+
+			m.Stop()
+		}
+		logger.Log.Info.Printf(logger.Message, logger.TagAgent, "Stopped all modules")
+	}()
+
+	for _, m := range modules {
+		logger.Log.Info.Printf(logger.Message, logger.TagAgent, fmt.Sprintf("Starting module %s", m.Title()))
+		go m.Start()
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT)
+
+	for {
+		select {
+		case sig := <-signalChan:
+			switch sig {
+			case syscall.SIGTERM, syscall.SIGINT:
+				logger.Log.Info.Printf(logger.Message, logger.TagAgent, "Got SIGTERM/SIGINT, exiting.")
+
+				return
+			}
+		}
+	}
+}
